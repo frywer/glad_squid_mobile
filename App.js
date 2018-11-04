c2a6d8450ef525663ce202c6c3ea8af4439170c7
marangoz <ivan.marangoz@easy.cz>
2018-11-04 14:18:35 +0100

    some protorype

--- App.js
+++ App.js
@@ -3,6 +3,24 @@ import React, { Component } from 'react';
 import { AppRegistry, View, Text, StyleSheet, ActivityIndicator, TouchableHighlight } from 'react-native';
 import Login from './Login';
 import AuthService from './AuthService';
+import ScrollableTabView from 'react-native-scrollable-tab-view';
+import TabBar from "react-native-underline-tabbar";
+import NavTabBar from 'react-native-nav-tabbar';
+
+const Page = ({label}) => (
+    <View style={styles.container}>
+      <Text style={styles.welcome}>
+        {label}
+      </Text>
+      <Text style={styles.instructions}>
+        To get started, edit index.ios.js
+      </Text>
+      <Text style={styles.instructions}>
+        Press Cmd+R to reload,{'\n'}
+        Cmd+D or shake for dev menu
+      </Text>
+    </View>
+);
 
 export default class GladSquidTest extends Component {
   constructor(props){
@@ -33,16 +51,50 @@ export default class GladSquidTest extends Component {
 
      if (this.state.isLoggedIn) {
        return (
-         <View style={styles.container}>
-           <Text style={styles.welcome}>Logged In</Text>
 
-           <TouchableHighlight onPress={this.onLogout.bind(this)} style={styles.button}>
-             <Text style={styles.buttonText}>
-               Log out
-             </Text>
-           </TouchableHighlight>
 
-         </View>
+
+          <NavTabBar>
+    <NavTabBar.Item
+        title="Home"
+    >
+        <View style={styles.container}>
+        <ScrollableTabView
+            tabBarActiveTextColor="#9565a1"
+            renderTabBar={() => <TabBar underlineColor="#9565a1" />}>
+          <Page tabLabel={{label: "Page #1"}} label="Page #1"/>
+          <Page tabLabel={{label: "Page #2 aka Long!", badge: 3}} label="Page #2 aka Long!"/>
+          <Page tabLabel={{label: "Page #3"}} label="Page #3"/>
+          <Page tabLabel={{label: "Page #4 aka Page"}} label="Page #4 aka Page"/>
+          <Page tabLabel={{label: "Page #5"}} label="Page #5"/>
+        </ScrollableTabView>
+        </View>
+    </NavTabBar.Item>
+    <NavTabBar.Item>
+        <View style={styles.textContent}>
+            <Text style={{fontSize: 18}}>Friends</Text>
+        </View>
+    </NavTabBar.Item>
+    <NavTabBar.Item
+        title="Settigs"
+    >
+        <View style={styles.container}>
+        <TouchableHighlight onPress={this.onLogout.bind(this)} style={styles.button}>
+          <Text style={styles.buttonText}>
+            Log out
+          </Text>
+        </TouchableHighlight>
+        </View>
+  </NavTabBar.Item>
+</NavTabBar>
+
+
+
+
+
+
+
+
        )
      } else {
        return(
@@ -67,6 +119,7 @@ export default class GladSquidTest extends Component {
 
 var styles = StyleSheet.create({
   container: {
+    paddingTop: 20,
     flex: 1,
     justifyContent: 'center',
     alignItems: 'center',
